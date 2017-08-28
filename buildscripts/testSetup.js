ffdae6464e8c581cfc54281bdd248d0a3f951194
KatherineEbel <kathyebel@me.com>
2017-08-28 12:18:52 -0400

    implement manipulating data via json server

--- buildscripts/testSetup.js
+++ buildscripts/testSetup.js
@@ -1,9 +1,5 @@
 'use strict';
 
-var _babelRegister = require('babel-register');
-
-var _babelRegister2 = _interopRequireDefault(_babelRegister);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+require('babel-register');
 
 require.extensions['.css'] = function () {};
--- src/api/userApi.js
+++ src/api/userApi.js
@@ -4,10 +4,15 @@ import { getBaseUrl } from './baseUrl'
 const baseUrl = getBaseUrl()
 
 /* eslint-disable no-console */
-console.log(baseUrl)
 const getUsers = () => get('users')
+const deleteUser = (id) => del(`users/${id}`)
 const get = (url) => fetch(baseUrl + url).then(onSuccess, onError)
+const del = (url) => {
+  const request = new Request(baseUrl + url, { method: 'DELETE' });
+  return fetch(request).then(onSuccess, onError)
+}
+
 const onSuccess = (response) => response.json()
 const onError = (error) => console.log(error)
 
-export { getUsers }
+export { getUsers, deleteUser }
